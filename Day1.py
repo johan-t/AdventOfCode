191865a845bd7681e1ef94138bdedbbb86f82dd0
Johan Trieloff <johan@trieloff.net>
2023-12-04 17:36:28 +0100

    added dictionary

--- Day1.py
+++ Day1.py
@@ -5,6 +5,19 @@ with open('input.text', 'r') as file:
     for line in file:
         EncodedInputlines.append(line.strip())
 
+dictionaryOfNumbers = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
 calibrationValues = []
 # Loop through the list and find the first and last digit of each line and concatenate them.
 for line in EncodedInputlines:
