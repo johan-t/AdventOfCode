f0c3ca86879e15f0e21eca2c4ddd87284cb53d92
Johan Trieloff <johan@trieloff.net>
2023-12-05 10:28:40 +0100

    not accounting for connecting words

--- Day1.py
+++ Day1.py
@@ -6,25 +6,34 @@ with open('input.text', 'r') as file:
         EncodedInputlines.append(line.strip())
 
 dictionaryOfNumbers = {
-    'zero': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
 }
 
-calibrationValues = []
-# Loop through the list and find the first and last digit of each line and concatenate them.
+# Create a new list for the strings of digits from each line
+modifiedDigits = []
 for line in EncodedInputlines:
-    digits = [char for char in line if char.isdigit()]
-    if digits:
+    # Replace the words with numbers
+    for key, value in dictionaryOfNumbers.items():
+        line = line.replace(key, value)
+
+    # Create a string to hold all digits from this line
+    digitString = ''.join([char for char in line if char.isdigit()])
+    modifiedDigits.append(digitString)
+
+calibrationValues = []
+# Loop through the modifiedDigits list
+for digitString in modifiedDigits:
+    if digitString:
         # Concatenate the first and last digit to form a two-digit number
-        calibrationValue = int(digits[0] + digits[-1])
+        calibrationValue = int(digitString[0] + digitString[-1])
         calibrationValues.append(calibrationValue)
 
 # Calculate the total sum of the calibration values
